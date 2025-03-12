e6042ba651da1aca6e125e56cbd8075ad0510fc3
Ahmed Abosalem <66332650+ahmedabosalem99@users.noreply.github.com>
2025-03-13 01:22:19 +0200

    Update Stack.cs

--- Stack/Stack.cs
+++ Stack/Stack.cs
@@ -1,63 +1,74 @@
+using System;
+using System.Text;
+
 namespace Data_Structure
 {
     public static class Constants
     {
-        public const int MAX_SIZE = 100;
+        // Theoretical maximum size of an array in .NET
+        public const int MAX_SIZE = int.MaxValue - 56;
     }
-    public class Stack
-    {
-        int top;
-        int[] item;
 
-        public Stack()
+    public class Stack<T>
+    {
+        private int top;
+        private readonly T[] item;
+
+        public Stack(int maxSize = Constants.MAX_SIZE)
         {
-            item = new int[Constants.MAX_SIZE];
+            if (maxSize <= 0)
+                throw new ArgumentException("Size must be greater than 0.");
+            if (maxSize > Constants.MAX_SIZE)
+                throw new ArgumentException($"Size exceeds the maximum allowed array size of {Constants.MAX_SIZE}.");
+
+            item = new T[maxSize];
             top = -1;
         }
 
-        public bool isEmpty() => top < 0;
+        public bool IsEmpty() => top < 0;
 
-        public bool isFull() => top >= Constants.MAX_SIZE - 1;
+        public bool IsFull() => top >= item.Length - 1;
 
-        public void Push(int element)
+        public void Push(T element)
         {
-            if (isFull())
-                Console.WriteLine("Stack is Full");
-            else
-                item[++top] = element;
+            if (IsFull())
+                throw new InvalidOperationException("Stack is full.");
+            item[++top] = element;
         }
 
-        public void Pop()
+        public T Pop()
         {
-            if (isEmpty())
-                Console.WriteLine("Stack is Empty");
-            else
-                top--;
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+            return item[top--];
         }
 
-        public void Pop(ref int element)
+        public T Peek()
         {
-            if (isEmpty())
-                Console.WriteLine("Stack is Empty");
-            else
-                element = item[top--];
-        }
-
-        public void Top(ref int stackTop)
-        {
-            if (isEmpty())
-                Console.WriteLine("Stack is Empty");
-            else
-                stackTop = item[top];
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+            return item[top];
         }
 
         public override string ToString()
         {
-            return string.Join(" ", item[0..(top + 1)].Reverse());
+            if (IsEmpty())
+                return "[]";
+
+            var result = new StringBuilder("[");
+            for (int i = top; i >= 0; i--)
+            {
+                result.Append(item[i]);
+                if (i > 0)
+                    result.Append(", ");
+            }
+            result.Append("]");
+            return result.ToString();
         }
+
         public void Print()
         {
-            Console.WriteLine($"[{ToString()}]");
+            Console.WriteLine(ToString());
         }
     }
 }
