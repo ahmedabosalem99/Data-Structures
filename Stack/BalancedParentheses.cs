f1571a16f42213c31897aeb672e770b9e0308421
Ahmed Abosalem <66332650+ahmedabosalem99@users.noreply.github.com>
2025-04-20 15:12:31 +0200

    Balanced Parentheses Using Stack {([ ])}

---
+++ Stack/BalancedParentheses.cs
@@ -0,0 +1,38 @@
+﻿//This implementation is based on our previous implemetation for the stack not the standard .NET Stack
+
+namespace Data_Structure.Stack
+{
+    public static class BalancedParentheses
+    {
+        public static bool CheckBalance(string expression)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach(char c in  expression)
+            {
+               switch(c)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                    case '}':
+                    case ']':
+                        if(stack.IsEmpty()  || !AreValidPair(stack.Pop(), c))
+                            return false;
+                        break;
+                }
+            }
+            return stack.IsEmpty();
+        }
+
+        private static bool AreValidPair(char left, char right)
+        {
+            return (left == '(' && right == ')')
+                || (left == '{' && right == '}')
+                || (left == '[' && right == ']');
+        }
+    }
+}
